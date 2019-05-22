2164fc794a71156b1d31ceae465bc7f0b391da10
Sydney Jeffries <sydney.jeffriess@gmail.com>
2019-05-22 15:23:07 -0500

    added validation errors

--- src/app/clothing/add-product/add-product.component.ts
+++ src/app/clothing/add-product/add-product.component.ts
@@ -11,15 +11,15 @@ import { Product } from 'src/app/model/product';
 })
 
 export class AddProductComponent implements OnInit {
-  public product: Product
-  
+  public product: Product;
+  public message: string;
   constructor() { 
     this.product = new Product("", 0, "URL");
   }
 
 validateform(formVarriable){
-  if (formVarriable.valid){
-    this.product = formVarriable.value.product
+  if (formVarriable.invalid){
+    this.message = 'you forgot something'
   }
 
 }
@@ -29,5 +29,28 @@ validateform(formVarriable){
   }
 
 
+
+}
+import { Component } from '@angular/core';
+import { Product } from 'app/model/product';
+
+@Component({
+  selector: 'app-create-product',
+  templateUrl: './create-product.component.html',
+  styleUrls: ['./create-product.component.css']
+})
+export class CreateProductComponent {
+
+  public message = '';
+  constructor() { }
+
+  createProduct(productForm) {
+    if (productForm.invalid) {
+      this.message = 'Please correct all errors and resubmit the form';
+    } else {
+      const product: Product = productForm.value.product;
+      console.log('Creating product', product);
+    }
+  }
 
 }
